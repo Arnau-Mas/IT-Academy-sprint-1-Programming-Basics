b4af21103e688fea93e56b7b8954a19439e3537d
Arnau-Mas <arnau.mava@gmail.com>
2021-11-24 11:01:35 +0100

    update

--- 1.2.classes-and-arrow-functions/nivell3ex1.js
+++ 1.2.classes-and-arrow-functions/nivell3ex1.js
@@ -1,26 +1,33 @@
 class Building {
-    constructor(kindOf, floors, meters2){
+    constructor(kindOf, name, floors, meters2){
         if(new.target === Building){
             throw new TypeError("Cannot construct Building instances directly");
         }
         this.kindOf = kindOf;
+        this.name = name;
         this.floors = floors;
         this.meters2 = meters2;
     }
 
-    prova(){
+    instanceTest(){
         console.log("Has aconseguit fer una instància");
     }
 }
 
-function Hospital(floors, meters2){
+function Hospital(name, floors, meters2){
     let hospital = Object.create(Building.prototype);
     hospital.kindOf = "Hospital";
+    hospital.name = name;
     hospital.floors = floors;
     hospital.meters2 = meters2;
     return hospital;
 }
 
-let hospital = new Hospital(23, 23);
-console.log(hospital);
-console.log(hospital.prova());
+let hospital1 = new Hospital("Sant Joan de Déu", 12, 4500);
+let hospital2 = new Hospital("Vall d'Hebrón", 20, 5000)
+
+console.log(hospital1);
+hospital1.instanceTest();
+
+console.log(hospital2);
+hospital2.instanceTest();
